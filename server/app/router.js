aca32c8c07c0c9b0a490319c5013c30518d129f3
AnnDo33 <ann.dominique.d@gmail.com>
2024-06-19 10:03:03 +0200

    mise en place route status

--- server/app/router.js
+++ server/app/router.js
@@ -9,16 +9,19 @@ const router = express.Router();
 
 // Import itemActions module for handling item-related operations
 const itemActions = require("./controllers/itemActions");
+const statusActions = require("./controllers/statusActions");
 // const client = require("../database/client");
 
 // Route to get a list of items
 router.get("/tasks", itemActions.browse);
+router.get("/status", statusActions.browse);
 
 // Route to get a specific item by ID
-// router.get("/items/:id", itemActions.read);
+router.get("/items/:id", itemActions.read);
 
 // Route to add a new item
 router.post("/tasks", itemActions.add);
+router.post("/status", statusActions.add);
 
 /* ************************************************************************* */
 
